ce376286e26858c9d3b56d64a00f38a834e18da9
khang0015 <thaidoankhang04@gmail.com>
2025-07-07 23:33:21 +0700

    Client

--- src/main/java/com/chat/app/controller/ChatController.java
+++ src/main/java/com/chat/app/controller/ChatController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ChatController {
 
-    @MessageMapping("/app/sendMessage")
+    @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
     public ChatMessage sendMessage(ChatMessage message){
         return message;
